9a3b6d1ef592db3f5f180cab0630099bd054cb86
daug <woshi_qianjun@163.com>
2019-05-29 16:54:37 +0800

    0529

--- src/components/listTable/ListTable.js
+++ src/components/listTable/ListTable.js
@@ -1,7 +1,31 @@
 import React,{Component,Fragment} from 'react';
 require('./ListTable.css');
+
+const userList = [
+    {'id':1,'name':'tom','dept':'IT'},
+    {'id':2,'name':'oom','dept':'IP'},
+    {'id':3,'name':'pom','dept':'IC'},
+    {'id':4,'name':'lom','dept':'IQ'},
+    {'id':5,'name':'mom','dept':'IE'},
+]
 class ListTable extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            list:userList
+        }
+    }
     render(){
+        const thList = 
+        const trList = this.state.list.map((item,index)=>{
+            return (
+                <tr>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.dept}</td>
+                </tr>
+            )
+        });
         return (
             <Fragment>
                 <table >
@@ -14,30 +38,7 @@ class ListTable extends Component{
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>2</td>
-                            <td>2</td>
-                            <td>3</td>
-                            <td>4</td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
+                        {trList}
                     </tbody>
                 </table>
             </Fragment>
